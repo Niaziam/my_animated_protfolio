898b26847b9193146e06af18a8d025c5c4d632f8
Niaziam <niazuddintanim20@gmail.com>
2025-01-01 16:17:10 +0600

    about section updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary-text text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm MD. Niaz Uddin, a web developer and designer with a passion for creating clean, efficient, and user-friendly applications. I'm skilled in frontend development and enjoy solving problems, whether it's fixing bugs or improving functionality. I'm also a strong team player with leadership experience, always eager to take on new challenges and learn along the way. My goal is to keep growing in this field and deliver high-quality, practical solutions that exceed client expectations.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap justify-center gap-10'>
--- src/constants/index.js
+++ src/constants/index.js
@@ -46,15 +46,15 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "React Developer",
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: "Next JS Developer",
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Web Designer",
     icon: creator,
   },
 ];
